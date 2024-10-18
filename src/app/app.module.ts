0f733f1cc02e3b51d019f8d8340e7e73ca980211
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-10-18 14:14:18 +0530

    angular 18 upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms'
 import { AuthGuard } from './guards/auth.guard';
 import { ProudctDetailsComponent } from './components/proudct-details/proudct-details.component';
@@ -17,25 +17,18 @@ import {FormsModule} from '@angular/forms'
 import { ProductServiceModule } from './modules/product-service/product-service.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    ProductListComponent,
-    ProudctDetailsComponent,
-    ProudctRatingComponent,
-    ProudctOffersComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    FormsModule,
-    ProductServiceModule
-  ],
-  providers: [AuthGuard],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        LoginComponent,
+        ProductListComponent,
+        ProudctDetailsComponent,
+        ProudctRatingComponent,
+        ProudctOffersComponent,
+        DashboardComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        ProductServiceModule], providers: [AuthGuard, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
